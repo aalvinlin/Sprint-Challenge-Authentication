1a1897c7973907dac585d4f43de860dd7f389b35
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-02-28 09:37:15 -0800

    MVP complete. authenticater-middleware verifies token before granting access to resource.

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,27 @@
+const jwt = require("jsonwebtoken");
+const jwtSecret = require("./jwtSecret");
 /* 
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+
+  const { authorization } = req.headers;
+
+  if (!authorization)
+    { res.status(400).json({ message: "You must log in first to view this resource."})}
+
+  else
+    {
+      jwt.verify(authorization, jwtSecret, (error, decodedToken) => {
+        if (error)
+          { res.status(403).json({message: "Invalid credentials."})}
+        else
+          {
+            req.decodedToken = decodedToken;
+            next();
+          }
+      })
+    }
 };
